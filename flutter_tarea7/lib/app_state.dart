03e73cae2d18e69b4abb7a72b236554f32ee613c
Javier Ochoa <51656728+ochoa18@users.noreply.github.com>
2024-05-31 16:32:29 -0600

    commit 5

--- flutter_tarea7/lib/app_state.dart
+++ flutter_tarea7/lib/app_state.dart
@@ -12,7 +12,50 @@ import 'guest_book_message.dart'; // new
 
 class ApplicationState extends ChangeNotifier {
   // Current content of ApplicationState elided ...
+  Future<void> init() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
 
+    FirebaseUIAuth.configureProviders([
+      EmailAuthProvider(),
+    ]);
+
+    FirebaseAuth.instance.userChanges().listen((user) {
+      if (user != null) {
+        _loggedIn = true;
+        _guestBookSubscription = FirebaseFirestore.instance
+            .collection('guestbook')
+            .orderBy('timestamp', descending: true)
+            .snapshots()
+            .listen((snapshot) {
+          _guestBookMessages = [];
+          for (final document in snapshot.docs) {
+            _guestBookMessages.add(
+              GuestBookMessage(
+                name: document.data()['name'] as String,
+                message: document.data()['text'] as String,
+              ),
+            );
+          }
+          notifyListeners();
+        });
+      } else {
+        _loggedIn = false;
+        _guestBookMessages = [];
+        _guestBookSubscription?.cancel();
+      }
+      notifyListeners();
+    });
+  }
+
+  bool _loggedIn = false;
+  bool get loggedIn => _loggedIn;
+
+  // Add from here...
+  StreamSubscription<QuerySnapshot>? _guestBookSubscription;
+  List<GuestBookMessage> _guestBookMessages = [];
+  List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
+  // ...to here.
   // Add from here...
   Future<DocumentReference> addMessageToGuestBook(String message) {
     if (!_loggedIn) {
