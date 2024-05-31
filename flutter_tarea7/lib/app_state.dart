61694c9e0bf94950b15d0256e23189128b735372
Javier Ochoa <51656728+ochoa18@users.noreply.github.com>
2024-05-31 16:24:47 -0600

    Commit 3

---
+++ flutter_tarea7/lib/app_state.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart'; // new
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+class ApplicationState extends ChangeNotifier {
+  // Current content of ApplicationState elided ...
+
+  // Add from here...
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (!_loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
+  // ...to here.
+}
