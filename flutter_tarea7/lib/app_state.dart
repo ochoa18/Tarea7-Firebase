bbecef5bac6e0ad308552973308cbdd074fa275e
Javier Ochoa <51656728+ochoa18@users.noreply.github.com>
2024-05-31 16:28:43 -0600

    Commit 4

--- flutter_tarea7/lib/app_state.dart
+++ flutter_tarea7/lib/app_state.dart
@@ -1,4 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart'; // new
+import 'dart:async'; // new
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
@@ -6,6 +8,7 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
+import 'guest_book_message.dart'; // new
 
 class ApplicationState extends ChangeNotifier {
   // Current content of ApplicationState elided ...
